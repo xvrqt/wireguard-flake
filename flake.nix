b7039efb1223ef58c0f7a29ca2b5eb3274803f18
xvrqt <git@xvrqt.com>
2025-07-26 15:59:24 -0700

    Made the configuration of the nixosModules automatic instead of manual.

--- flake.nix
+++ flake.nix
@@ -5,43 +5,63 @@
   outputs =
     { secrets, ... }:
     let
+      names = [ "lighthouse" "archive" "spark" "third-lobe" ];
       machines = import ./machines.nix;
+
+      # Keeping things DRY
+      cfg = { lib, name, config, ... }: {
+        imports = [
+          # Needs the secrets module to function since we will be deploying
+          # Wireguard private keys on each machine
+          secrets.nixosModules.default
+          # Configure the Wireguard interface
+          (import ./wireguard { inherit lib name config machines; })
+        ];
+      };
     in
     {
-      nixosModules = {
-        archive = { lib, config, ... }:
-          let
-            name = "archive";
-            inherit machines;
-          in
-          {
-            imports = [
-              secrets.nixosModules.default
-              (import ./wireguard { inherit lib name config machines; })
-            ];
-          };
-        lighthouse = { lib, config, ... }:
-          let
-            name = "lighthouse";
-            inherit machines;
-          in
-          {
-            imports = [
-              secrets.nixosModules.default
-              (import ./wireguard { inherit lib name config machines; })
-            ];
-          };
-        spark = { lib, config, ... }:
-          let
-            name = "spark";
-            inherit machines;
-          in
-          {
-            imports = [
-              secrets.nixosModules.default
-              (import ./wireguard { inherit lib name config machines; })
-            ];
-          };
-      };
+      # For each 'name' in 'names'
+      # Create an attribute set key and, create a value for that key by calling
+      # cfg() using that name as a parameter
+      # e.g. {
+      #   nyaa = { /* NixOS Module Config */ };
+      #   spark = { /* NixOS Module Config */ };
+      #   ...
+      # }
+      nixosModules = builtins.listToAttrs (
+        map
+          (item: {
+            name = item;
+            value = { lib, config, ... }: cfg { inherit lib config; name = item; };
+          })
+          names
+      );
+      #   {
+      #   archive = { lib, config, ... }:
+      #     let
+      #       name = "archive";
+      #       inherit machines;
+      #     in
+      #     {
+      #       imports = [
+      #         secrets.nixosModules.default
+      #         (import ./wireguard { inherit lib name config machines; })
+      #       ];
+      #     };
+      #   lighthouse = { lib, config, ... }:
+      #     let
+      #       name = "lighthouse";
+      #       inherit machines;
+      #     in
+      #     {
+      #       imports = [
+      #         secrets.nixosModules.default
+      #         (import ./wireguard { inherit lib name config machines; })
+      #       ];
+      #     };
+      #   spark = { lib, config, ... }: cfg {
+      #     inherit lib config; name = "spark";
+      #   };
+      # };
     };
 }
---
+++ wireguard/nyaa.nix
@@ -0,0 +1,24 @@
+{ pka, port, machines, ... }:
+[
+  rec {
+    name = "lighthouse";
+    endpoint = "${machines.${name}.ip.v4.www}:${toString port}";
+    publicKey = "${machines.${name}.wg.publicKey}";
+    allowedIPs = [ "${machines.${name}.ip.v4.wg}/32" ];
+    persistentKeepalive = pka;
+  }
+  rec {
+    name = "spark";
+    endpoint = "${machines.${name}.ip.v4.local}:${toString port}";
+    publicKey = "${machines.${name}.wg.publicKey}";
+    allowedIPs = [ "${machines.${name}.ip.v4.wg}/32" ];
+    persistentKeepalive = pka;
+  }
+  rec {
+    name = "archive";
+    endpoint = "${machines.${name}.ip.v4.local}:${toString port}";
+    publicKey = "${machines.${name}.wg.publicKey}";
+    allowedIPs = [ "${machines.${name}.ip.v4.wg}/32" ];
+    persistentKeepalive = pka;
+  }
+]
